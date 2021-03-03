57c40c1b014b8b6408d1485acf9b2d7d620f9230
Quoding <quoding@protonmail.ch>
2021-03-02 19:04:23 -0500

    Add !genimg command to generate abstract art on demande

--- bot.py
+++ bot.py
@@ -3,7 +3,6 @@
 """bot.py : main Python file for Cobalt bot"""
 
 __author__ = "Quoding"
-__license__ = "GPL"
 __version__ = "0.1.0"
 __maintainer__ = "Quoding"
 __email__ = "quoding@protonmail.ch"
@@ -12,7 +11,8 @@ import os
 import discord
 from discord.ext import commands
 from dotenv import load_dotenv
-from polls import make_poll
+from python_abstract_art.main import make_image
+import io
 
 load_dotenv()
 
@@ -26,22 +26,14 @@ async def on_ready():
     print(f"{bot.user} has connected to Discord")
 
 
-@bot.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(f"Hey {member.name}, take a walk on the left side.")
+@bot.command(help="Generate a dope-ass abstract picture, yo!")
+async def genimg(ctx):
+    img = make_image()
 
-
-@bot.command(name="poll", help="Create a poll. Usage: !poll [days, yesno]")
-async def create_poll(ctx, preset):
-    await ctx.message.delete()
-
-    emojis, response = make_poll(preset)
-
-    msg = await ctx.send(response)
-
-    for emoji in emojis:
-        await msg.add_reaction(emoji)
+    with io.BytesIO() as image_binary:
+        img.save(image_binary, "PNG")
+        image_binary.seek(0)
+        await ctx.send(file=discord.File(fp=image_binary, filename="image.png"))
 
 
 bot.run(TOKEN)
